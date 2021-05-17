5c3b63fe446d46c2c8a4fd844c25e36ddeef64f7
wcoelho <wcoelho@esec.com.br>
2021-05-17 20:04:19 -0300

    final

--- src/WordFrequencyController.java
+++ src/WordFrequencyController.java
@@ -16,8 +16,17 @@ public class WordFrequencyController {
     }
 
     private void init(){
+        if (word in self._word_freqs){
+            self._word_freqs[word] += 1;
+        }
+        else{
+            self._word_freqs[word] = 1
+        }
 
     }*/
+
+    /*def _sorted(self):
+        return sorted(self._word_freqs.items(), key=operator.itemgetter(1), reverse=True)*/
 
 
 
--- src/WordFrequencyManager.java
+++ src/WordFrequencyManager.java
@@ -1,3 +1,43 @@
 public class WordFrequencyManager {
+
+    private Object storage_manager;
+    private Object stop_word_manager;
+    private Object word_freq_manager;
+
+    public Object dispatch(String[] message){
+        if(message[0] == "init"){
+            init(message[1]);
+        }
+        else if(message[0] == "run"){
+
+        }else{
+            return null;
+        }
+        return null;
+    }
+
+    private void init(String pathToFile){
+        /*storage_manager = DataStorageManager();
+        stop_word_manager = StopWordManager();
+        word_freq_manager = WordFrequencyManager();
+        storage_manager.dispatch(['init', pathToFile])
+        stop_word_manager.dispatch(['init'])*/
+    }
+
+    private void run(){
+        while(Object w == storage_manager.dispatch(['words'])){
+            if(!stop_word_manager.dispatch(['is_stop_word', w])){
+                word_freq_manager.dispatch(['increment_count', w]);
+            }
+        }
+        word_freqs = self._word_freq_manager.dispatch(['sorted'])
+        Object c;
+        for(int i = 0; i < 25; i++){
+            if(w == word_freqs[i] || c == word_freqs[i]){
+                System.out.println(w + "-" + c);
+            }
+        }
+    }
+        
     
 }
